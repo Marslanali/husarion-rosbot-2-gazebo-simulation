953867171e35b59357f963d079f2947cb5672fec
marslanali <arslanali800@hotmail.com>
2020-04-06 02:23:53 +0500

    removing build

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/arslanali/husarion-rosbot-2-gazebo-simulation/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/arslanali/husarion-rosbot-2-gazebo-simulation/devel;/home/arslanali/TurtleBot3/devel;/opt/ros/kinetic".split(';') if "/home/arslanali/husarion-rosbot-2-gazebo-simulation/devel;/home/arslanali/TurtleBot3/devel;/opt/ros/kinetic" != "" else []
